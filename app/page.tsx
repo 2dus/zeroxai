d86ba13d9ce6c2a1a74a2703d7e720d2d42e97cc
2dus <hunnid@tutanota.com>
2025-01-25 22:54:11 +0100

    Update layout and page config

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import ZeroxChat from '@/components/ZeroxChat'
 
-export default function Page() {
- return (
-   <main className="min-h-screen">
-     <ZeroxChat />
-   </main>
- )
+export const dynamic = 'force-dynamic'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <ZeroxChat />
+    </main>
+  )
 }
